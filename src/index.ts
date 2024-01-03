f7e8f51cbb182f23b1eb252116c6e0bb653f4a29
Shohei Kondo <s.kondo1015@gmail.com>
2024-01-03 15:35:54 +0900

    ver2

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello konsho sandbox Hono!')
+  return c.text('Hello konsho sandbox Hono! ver.2')
 })
 
 export default app
